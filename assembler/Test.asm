b942971db66a475fb7dc11c8103697fe1186ae34
Ionică Bizău <bizauionica@yahoo.com>
2014-12-10 11:17:54 +0200

    testing

--- assembler/Test.asm
+++ assembler/Test.asm
@@ -1,13 +1,6 @@
-!
-! A simple ARC program to add two numbers
-!
-      .begin
-      .org 2048
-main: ld [x], %r1               ! load x into %r1 | 15
-      st %r1, [y]               ! store %r1 into [y]
-      ld [y], %r2               ! load y into %r2 | 15
-      jmpl %r15+4, %r0          ! standard return
-x:    -15
-z:    -0x1bb
-y:    0
-      .end
+        .begin
+        .org 2048
+main:   ld [x], %r1
+        jmpl %r15+4, %r0
+x:      10
+        .end
