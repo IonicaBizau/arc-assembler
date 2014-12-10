c8e496b0ebbbe8398cf5d52f4e70c27b9a7dc2d2
Ionică Bizău <bizauionica@yahoo.com>
2014-12-10 19:38:54 +0200

    Fixed register name

--- assembler/Test.asm
+++ assembler/Test.asm
@@ -8,14 +8,15 @@
 main:   ld [x], %r9                 ! load x into r9
         ld [y], %r10                ! load y into r10
         call sum                    ! call sum subrutine
-        st %r10, [z]                ! store the result in z
+        st %r11, [z]                ! store the result in z
+        ld [z], %r9
         jmpl %r15+4, %r0            ! Done
 
 sum:    addcc %r9, %r10, %r11       ! sums the registers r9 and r10 into %r11
         jmpl %r15+4, %r0            ! standard return
 
 x:      10
-y:      20
+y:      -20
 z:      0
 
         .end
