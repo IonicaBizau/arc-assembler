ef99289aac1d5f0f30b059a1050e8374ecbdf8af
Ionică Bizău <bizauionica@yahoo.com>
2014-12-10 11:17:18 +0200

    Absolute path to binary file

--- interpreter/index.js
+++ interpreter/index.js
@@ -3,7 +3,7 @@ var Fs = require("fs")
   , Interpreter = require("./lib")
   ;
 
-const INPUT_FILE = Path.resolve("../assembler/out");
+const INPUT_FILE = Path.resolve(__dirname + "/../assembler/out");
 Fs.readFile(INPUT_FILE, function (err, buff) {
     if (err) { throw err; }
     console.log(Interpreter.interpret(buff));
