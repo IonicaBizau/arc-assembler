bb5a97da58d59756cee3b4d755f694ab2cf5895c
Ionică Bizău <bizauionica@yahoo.com>
2014-12-11 17:56:05 +0200

    Show the registers

--- interpreter/index.js
+++ interpreter/index.js
@@ -7,5 +7,12 @@ const INPUT_FILE = Path.resolve(__dirname + "/../assembler/out");
 Fs.readFile(INPUT_FILE, function (err, buff) {
     if (err) { throw err; }
     console.log(Interpreter.interpret(buff));
+    console.log("--------------");
+    console.log("Registers: ");
+    var reg = {};
+    Object.keys(Interpreter.r).forEach(function (c, i) {
+        reg[Interpreter.registerMap[c]] = Interpreter.r[c];
+    });
+    console.log(JSON.stringify(reg, null, 4));
 });
 
