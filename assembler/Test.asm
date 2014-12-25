a4717d886d0c3de783fcc11d44f2707870ab6bfe
Ionică Bizău <bizauionica@yahoo.com>
2014-12-25 10:48:30 +0200

    Another test

--- assembler/Test.asm
+++ assembler/Test.asm
@@ -1,32 +1,11 @@
-!
-! This program calculates the product of two positive numbers
-!
-
-        .begin
-        .org 2048
-
-main:       ld [x], %r10            ! load x into r10
-            ld [y], %r11            ! load y into r11
-            ld [minus_one], %r14    ! r14 = -1
-            call pre_mult
-            call mult               ! call the mult subrutine
-            jmpl %r15+4, %r0        ! Done
-
-pre_mult:   addcc %r11, %r14, %r11
-            jmpl %r15+4, %r0        ! Done
-
-mult:       addcc %r10, %r10, %r10  ! x += x
-            addcc %r11, %r14, %r11
-            orncc %r0, %r11, %r16   ! check if r11 is 0
-            be mult
-            jmpl %r15+4, %r0
-
-m_tmp: 0
-minus_one: -1
-plus_one: 1
-
-x:      5
-y:      4
-z:      0
-
-        .end
+            .begin
+            .org 2048
+            ld [length], %r1
+            ld [address], %r2
+            andcc %r3, %r0, %r3
+            andcc %r1, %r1, %r0
+loop:       jmpl %r15+4, %r0
+            .org 3000
+length:     20
+address:    3000
+            .end
