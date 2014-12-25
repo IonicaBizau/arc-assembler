e56ee3b79eba6c51e0f1d8803917793329c6f940
Ionică Bizău <bizauionica@yahoo.com>
2014-12-25 11:47:04 +0200

    Updated client files

--- assembler/Test.asm
+++ assembler/Test.asm
@@ -1,11 +1,23 @@
-            .begin
-            .org 2048
-            ld [length], %r1
-            ld [address], %r2
-            andcc %r3, %r0, %r3
-            andcc %r1, %r1, %r0
-loop:       jmpl %r15+4, %r0
-            .org 3000
-length:     20
-address:    3000
-            .end
+        .begin
+        .org 2048
+        ld [length], %r1
+        ld [zero], %r10
+        ld [start], %r4
+        call loop
+        jmpl %r15+4, %r0
+
+loop:   addcc %r4, 4, %r4
+        ld %r4, %r10
+
+        addcc %r1, -1, %r1
+        jmpl %r15+4, %r0
+
+length: 3
+zero:   0
+start:  3000
+
+        .org 3000
+        1
+        2
+        3
+        .end
