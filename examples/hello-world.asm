d1c548929ef6e93885d6a4e2a9c33cf67f05b036
Ionică Bizău <bizauionica@yahoo.com>
2014-12-31 11:28:32 +0200

    Comments

--- examples/hello-world.asm
+++ examples/hello-world.asm
@@ -1,10 +1,19 @@
-! Hello World
+! ======================================= !
+! Hello World Program written in Assembly !
+! --------------------------------------- !
+! This program prints in console the text !
+! "Hello World"                           !
+! ======================================= !
+
             .begin
             .org 2048
+
 main:       ld [h_start], %r1
             ld [length], %r2
             call loop
 
+            ! Load characters one by one
+            ! and print them in the console
 loop:       ld %r1, %r3
             addcc %r1, 4, %r1
             addcc %r2, -1, %r2
@@ -16,6 +25,8 @@ done:       jmpl %r15+4, %r0
 
 h_start:    3000
 length:     12
+
+            ! "Hello World" ASCII codes
             .org 3000
             72
             101
