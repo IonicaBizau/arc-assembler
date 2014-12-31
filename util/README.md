1dd7fabac06f717753876133c7538ba70b31c5d3
Ionică Bizău <bizauionica@yahoo.com>
2014-12-31 10:32:20 +0200

    JSDoc comments and documentation for arc-util

--- util/README.md
+++ util/README.md
@@ -2,3 +2,78 @@ Utility Package for ARC Assembler & Interpreter
 ===============================================
 
 The utility package for [ARC Assembler & Interpreter](https://github.com/IonicaBizau/arc-assembler).
+
+# Documentation
+
+## `pad(input, l, c)`
+Padds the input.
+
+### Params
+- **String** `input`: The value that should be padded.
+- **Number** `l`: The pad length (default: `32`).
+- **String** `c`: Pad content (default: `"c"`).
+
+### Return
+- **String** The padded input.
+
+## `isRegister(inp)`
+Checks if the input is a register.
+
+### Params
+- **String** `inp`: The input value.
+
+### Return
+- **Boolean** A boolean value representing whether the input is a register or not.
+
+## `isLocAdd(inp)`
+Checks if the input is a location address.
+
+### Params
+- **String** `inp`: The input value.
+
+### Return
+- **Boolean** A boolean value representing whether the input is a location address or not.
+
+## `addBin()`
+Sums the numbers provided in parameters.
+
+### Return
+- **String** The binary sum of provided arguments.
+
+## `comp(input)`
+Runs the complementary task.
+
+### Params
+- **String** `input`: The input value.
+
+### Return
+- **String** The result value.
+
+## `uncomp(input)`
+Computes the decimal value of the input value in two's complement input.
+
+### Params
+- **String** `input`: The input value.
+
+### Return
+- **Number** The decimal value of the two's complement input.
+
+## `bin(input, l)`
+Converts a decimal value to binary.
+
+### Params
+- **String** `input`: The input value.
+- **Number** `l`: The number of bits.
+
+### Return
+- **String** The input value in binary.
+
+## `isNumber(c)`
+Checks if the input is a number or not.
+
+### Params
+- **String** `c`: The input value.
+
+### Return
+- **Boolean** A boolean value representing whether the input is a valid number or not.
+
--- util/index.js
+++ util/index.js
@@ -1,4 +1,17 @@
+// Constructor
 var Util = module.exports = {};
+
+/**
+ * pad
+ * Padds the input.
+ *
+ * @name pad
+ * @function
+ * @param {String} input The value that should be padded.
+ * @param {Number} l The pad length (default: `32`).
+ * @param {String} c Pad content (default: `"c"`).
+ * @return {String} The padded input.
+ */
 Util.pad = function (input, l, c) {
     c = c || "0";
     l = l || 32;
@@ -6,26 +19,70 @@ Util.pad = function (input, l, c) {
     return (pad + input).slice(-l);
 };
 
+/**
+ * isRegister
+ * Checks if the input is a register.
+ *
+ * @name isRegister
+ * @function
+ * @param {String} inp The input value.
+ * @return {Boolean} A boolean value representing whether the input is a register or not.
+ */
 Util.isRegister = function (inp) {
     return /^\%r[0-9]+$/.test(inp);
 };
 
+/**
+ * isLocAdd
+ * Checks if the input is a location address.
+ *
+ * @name isLocAdd
+ * @function
+ * @param {String} inp The input value.
+ * @return {Boolean} A boolean value representing whether the input is a location address or not.
+ */
 Util.isLocAdd = function (inp) {
     return /^\[[a-z,_]+\]$/.test(inp);
 };
 
+/**
+ * addBin
+ * Sums the numbers provided in parameters.
+ *
+ * @name addBin
+ * @function
+ * @return {String} The binary sum of provided arguments.
+ */
 Util.addBin = function () {
     var r = 0;
     for (var i = 0; i < arguments.length; ++i) {
-        r += parseInt(arguments[i], 2)
+        r += Util.uncomp(arguments[i]);
     }
-    return r.toString(2);
+    return Util.comp(r);
 };
 
+/**
+ * comp
+ * Runs the complementary task.
+ *
+ * @name comp
+ * @function
+ * @param {String} input The input value.
+ * @return {String} The result value.
+ */
 Util.comp = function (input) {
     return input.replace(/1/g, "a").replace(/0/g, "1").replace(/a/g, "0");
 };
 
+/**
+ * uncomp
+ * Computes the decimal value of the input value in two's complement input.
+ *
+ * @name uncomp
+ * @function
+ * @param {String} input The input value.
+ * @return {Number} The decimal value of the two's complement input.
+ */
 Util.uncomp = function (input) {
     if (input[0] === "1") {
         return -Util.uncomp(Util.comp(input)) - 1;
@@ -33,6 +90,16 @@ Util.uncomp = function (input) {
     return parseInt(input, 2);
 };
 
+/**
+ * bin
+ * Converts a decimal value to binary.
+ *
+ * @name bin
+ * @function
+ * @param {String} input The input value.
+ * @param {Number} l The number of bits.
+ * @return {String} The input value in binary.
+ */
 Util.bin = function (input, l) {
     if (input >= 0) {
         return Util.pad(input.toString(2), l);
@@ -40,6 +107,15 @@ Util.bin = function (input, l) {
     return Util.comp(Util.bin(Math.abs(input + 1), l));
 };
 
+/**
+ * isNumber
+ * Checks if the input is a number or not.
+ *
+ * @name isNumber
+ * @function
+ * @param {String} c The input value.
+ * @return {Boolean} A boolean value representing whether the input is a valid number or not.
+ */
 Util.isNumber = function (c) {
     return /(^\-?[0-9]+$)|(^\-?0x)|H$/.test(c);
 }
